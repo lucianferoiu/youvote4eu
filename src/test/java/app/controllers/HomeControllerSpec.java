75f76a30f331bda05236e3fea5ac8a14776744b1
Lucian Feroiu <lucianferoiu@gmail.com>
2015-04-15 20:37:27 +0300

    Dumb Spec tests need at least one @Test method :)

--- src/test/java/app/controllers/HomeControllerSpec.java
+++ src/test/java/app/controllers/HomeControllerSpec.java
@@ -1,10 +1,16 @@
 package app.controllers;
 
 import org.javalite.activeweb.ControllerSpec;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HomeControllerSpec extends ControllerSpec {
 
 	final static Logger log = LoggerFactory.getLogger(HomeControllerSpec.class);
+
+	@Test
+	public void basicRoundrip() throws Exception {
+		request().get("index");
+	}
 }
