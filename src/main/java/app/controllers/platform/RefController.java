723419c49088c5a10a842fa0b8da1a183c115c15
Lucian Feroiu <lucianferoiu@gmail.com>
2015-07-20 15:06:09 +0300

    Logging the Partner actions

--- src/main/java/app/controllers/platform/RefController.java
+++ src/main/java/app/controllers/platform/RefController.java
@@ -3,6 +3,8 @@ package app.controllers.platform;
 import java.util.Set;
 
 import org.javalite.activeweb.annotations.GET;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import app.base.Const;
 import app.base.PlatformController;
@@ -14,6 +16,7 @@ import app.models.Tag;
  * JSON-centric service for reference data retrieval
  */
 public class RefController extends PlatformController {
+	public static final Logger log = LoggerFactory.getLogger(RefController.class);
 
 	@GET
 	public void langs() {
@@ -34,6 +37,7 @@ public class RefController extends PlatformController {
 		Partner me = (Partner) session(Const.AUTHENTICATED_PARTNER);
 		final String[] EXCLUDED_FIELDS = { "password", "auth_token", "created_at", "updated_at" };
 		if (me == null) {
+			log.warn("No authenticated partner on session..");
 			json_404("session corrupted: no authenticated user");//this could very well be 403 - how did this call got through?!
 			return;
 		}
--- src/main/java/app/controllers/platform/StatisticsController.java
+++ src/main/java/app/controllers/platform/StatisticsController.java
@@ -1,8 +1,13 @@
 package app.controllers.platform;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import app.base.PlatformController;
 
 public class StatisticsController extends PlatformController {
+
+	public static final Logger log = LoggerFactory.getLogger(StatisticsController.class);
 
 	public void index() {}
 
