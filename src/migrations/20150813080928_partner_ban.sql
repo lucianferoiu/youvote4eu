79130c70cff18e331ffb6b8988b5182ac6406008
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-18 15:45:58 +0300

    Commenting out the code

--- src/migrations/20150813080928_partner_ban.sql
+++ src/migrations/20150813080928_partner_ban.sql
@@ -1,3 +1,4 @@
+/** Add the banning of partner information **/
 ALTER TABLE partners ADD COLUMN banned_at TIMESTAMP WITH TIME ZONE;
 ALTER TABLE partners ADD COLUMN ban_reason VARCHAR(4098);
 ALTER TABLE partners ADD COLUMN banned_by BIGINT;
