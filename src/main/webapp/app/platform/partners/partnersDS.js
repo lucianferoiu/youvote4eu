46c95b5651dc9f57a8e44d2a1e7eeb5a66390ba2
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-19 11:01:30 +0300

    Added data service functions for partner banning - issue #3

--- src/main/webapp/app/platform/partners/partnersDS.js
+++ src/main/webapp/app/platform/partners/partnersDS.js
@@ -1,7 +1,7 @@
 (function() {
 	angular.module('app.partners')
 	.service('partnersDS',['$q','$http',PartnersDS]);//we want a new instance per client (so we use service instead of factory) so we can maintain state inside it...
-		
+
 		function PartnersDS($q,$http) {
 			var ds = {
 				PAGE_SIZE: 10,
@@ -13,10 +13,10 @@
 				getPartners: getPartners,
 				getPartnerById: getPartnerById,
 				savePartner: savePartner
-				
+
 			};
 		//----------------------------------------------//
-			
+
 			function getPartners(page,sort,onSuccess,onError) {
 				var cfg = {
 					params: {
@@ -29,20 +29,20 @@
 					.success(function(data, status, headers, config) {
 						ds.total = data.total;
 						onSuccess(data.results);
-					}) 
+					})
 					.error(function(data, status, headers, config) {
 						onError(data);
-					}); 
+					});
 			}
 
 			function countPages() {
 				return Math.ceil(ds.total/ds.PAGE_SIZE);
 			}
-			
+
 			function countPartners() {
 				return Math.max(ds.total,0);
 			}
-			
+
 			//----------------------------------------------//
 
 			function getPartnerById(partnerId,onSuccess,onError) {
@@ -54,23 +54,56 @@
 				$http.get('/platform/partners/edit',cfg)
 					.success(function(data, status, headers, config) {
 						onSuccess(data);
-					}) 
+					})
 					.error(function(data, status, headers, config) {
 						onError(data);
 					});
 			}
-			
-			
+
+
 			function savePartner(partner,onSuccess,onError) {
 				var cfg = {};
 				$http.post('/platform/partners/save',partner,cfg)
 					.success(function(data, status, headers, config) {
 						onSuccess(data);
-					}) 
+					})
 					.error(function(data, status, headers, config) {
 						onError(data);
 					});
 				}
-			
+
+				function messagePartner(partnerId,message,onSuccess,onError) {
+					var cfg = {
+						params: {
+							id: partnerId,
+							message: message
+						}
+					};
+					$http.post('/platform/partners/message',partnerId,cfg)
+						.success(function(data, status, headers, config) {
+							onSuccess(data);
+						})
+						.error(function(data, status, headers, config) {
+							onError(data);
+						});
+				}
+
+				function banPartner(partnerId,message,onSuccess,onError) {
+					var cfg = {
+						params: {
+							id: partnerId,
+							message: message
+						}
+					};
+					$http.post('/platform/partners/ban',partnerId,cfg)
+						.success(function(data, status, headers, config) {
+							onSuccess(data);
+						})
+						.error(function(data, status, headers, config) {
+							onError(data);
+						});
+				}
+
+
 		}
 }());
