54c9a98e5ec6c938bf25741e56e509672443e6a2
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-27 00:48:05 +0300

    Formatting..

--- src/migrations/20150410170243_create_citizen.sql
+++ src/migrations/20150410170243_create_citizen.sql
@@ -2,9 +2,9 @@
 CREATE TABLE IF NOT EXISTS citizens (
 	id BIGSERIAL PRIMARY KEY,
 	/* - */
-	country CHAR(2),								/* if the citizen chose the country - used exclusively for statistics */
+	country CHAR(2),											/* if the citizen chose the country - used exclusively for statistics */
 	lang CHAR(2) DEFAULT 'EN',						/* preferred language of the citizen (null defaults to English) */
-	validated BOOLEAN DEFAULT false,				/* the citizen authenticated his/her presence (i.e. via email) */
+	validated BOOLEAN DEFAULT false,			/* the citizen authenticated his/her presence (i.e. via email) */
 	/* - */
 	created_at TIMESTAMP WITH TIME ZONE,
 	updated_at TIMESTAMP WITH TIME ZONE
