23d72322a5cc110f2082d4c4f7f6d5d4e78c7361
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-27 00:47:54 +0300

    Formatting..

--- src/migrations/20150604132633_create_questions_tags.sql
+++ src/migrations/20150604132633_create_questions_tags.sql
@@ -2,8 +2,8 @@
 CREATE TABLE IF NOT EXISTS questions_tags (
 	id BIGSERIAL PRIMARY KEY,
 	/* - */
-	question_id BIGINT NOT NULL,					/* the FK of the parent question */
-	tag_id BIGINT NOT NULL,							/* the FK of the parent tag */
+	question_id BIGINT NOT NULL,							/* the FK of the parent question */
+	tag_id BIGINT NOT NULL,										/* the FK of the parent tag */
 	created_by BIGINT NOT NULL,								/* the platform partner that tagged the question */
 	/* - */
 	created_at TIMESTAMP WITH TIME ZONE,
