67681635ecb205bf76b799923b6f05a95bdc97fb
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-28 04:13:19 +0300

    Formatting..

--- src/migrations/20150413123348_create_country.sql
+++ src/migrations/20150413123348_create_country.sql
@@ -2,13 +2,13 @@
 CREATE TABLE IF NOT EXISTS countries (
 	id BIGSERIAL PRIMARY KEY,
 	/* - */
-	code CHAR(2) NOT NULL,							/* ISO 3166-1 alpha-2 two-letter code of the country */
+	code CHAR(2) NOT NULL,								/* ISO 3166-1 alpha-2 two-letter code of the country */
 	is_eu BOOLEAN DEFAULT false,					/* is this country an EU member state? */
-	label VARCHAR(120),								/* label of the country (english version) */
-	preferred_lang CHAR(2) DEFAULT 'en',			/* preferred language of the country (null or 'en' if multi-language country) */
+	label VARCHAR(120),										/* label of the country (english version) */
+	preferred_lang CHAR(2) DEFAULT 'en',	/* preferred language of the country (null or 'en' if multi-language country) */
 	/* - */
 	created_at TIMESTAMP WITH TIME ZONE,
 	updated_at TIMESTAMP WITH TIME ZONE
 	/*		- see translations for the one-to-many polymorphic translations of the label */
-	
+
 );
