05099eacfd56ba1271e07171497a8d7906ae6f0f
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-28 04:14:05 +0300

    Formatting..

--- src/migrations/20150413134231_create_vote.sql
+++ src/migrations/20150413134231_create_vote.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS votes (
 	/* - */
 	question_id BIGINT NOT NULL,					/* the question on which a vote was cast - FK of the parent question */
 	citizen_id BIGINT NOT NULL,						/* the citizen that voted on the question - FK of the parent citizen */
-	value SMALLINT NOT NULL,						/* value of the vote - normally 0=No, 1=Yes, but we use smallint instead of boolean for possible multi-choice questions in the future */
-	validated BOOLEAN DEFAULT false,				/* the vote is validated when the citizen is (i.e. via email) */
-	cast_at TIMESTAMP WITH TIME ZONE,				/* the vote was cast at the specified date/time */
+	value SMALLINT NOT NULL,							/* value of the vote - normally 0=No, 1=Yes, but we use smallint instead of boolean for possible multi-choice questions in the future */
+	validated BOOLEAN DEFAULT false,			/* the vote is validated when the citizen is (i.e. via email) */
+	cast_at TIMESTAMP WITH TIME ZONE,			/* the vote was cast at the specified date/time */
 	/* - */
 	created_at TIMESTAMP WITH TIME ZONE,
 	updated_at TIMESTAMP WITH TIME ZONE
