eb1e9db499b94710a86ec1ddfef3280cc3806151
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-29 09:12:51 +0300

    Formatting..

--- src/migrations/20150813080928_partner_ban.sql
+++ src/migrations/20150813080928_partner_ban.sql
@@ -1,4 +1,9 @@
 /** Add the banning of partner information **/
-ALTER TABLE partners ADD COLUMN banned_at TIMESTAMP WITH TIME ZONE;
-ALTER TABLE partners ADD COLUMN ban_reason VARCHAR(4098);
-ALTER TABLE partners ADD COLUMN banned_by BIGINT;
+ALTER TABLE partners
+	ADD COLUMN banned_at TIMESTAMP WITH TIME ZONE;
+
+ALTER TABLE partners
+	ADD COLUMN ban_reason VARCHAR(4098);
+
+ALTER TABLE partners
+	ADD COLUMN banned_by BIGINT;
