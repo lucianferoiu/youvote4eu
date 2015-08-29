203a679c0d2cdd0be77d9f94eda111338b9f5717
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-29 09:12:21 +0300

    Formatting..

--- src/migrations/20150722092512_translations_validation.sql
+++ src/migrations/20150722092512_translations_validation.sql
@@ -1,7 +1,7 @@
 /* Flag for partner's ability to validate and lock/unlock a translation for a question */
 ALTER TABLE partners
-ADD COLUMN can_validate_translation BOOLEAN DEFAULT false;
+	ADD COLUMN can_validate_translation BOOLEAN DEFAULT false;
 
 /* A flag marking a translation as verified */
 ALTER TABLE translations
-ADD COLUMN verified BOOLEAN DEFAULT false;
+	ADD COLUMN verified BOOLEAN DEFAULT false;
