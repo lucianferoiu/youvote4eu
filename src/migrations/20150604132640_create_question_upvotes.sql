4c405217cf23e4be94efd4b960efe17074611dfe
Lucian Feroiu <lucianferoiu@gmail.com>
2015-08-29 09:11:46 +0300

    Formatting..

--- src/migrations/20150604132640_create_question_upvotes.sql
+++ src/migrations/20150604132640_create_question_upvotes.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS upvotes (
 	/* - */
 	question_id BIGINT NOT NULL,					/* the FK of the parent question */
 	upvoted_by BIGINT NOT NULL,						/* the platform partner upvoted (supported) the question */
-	value SMALLINT DEFAULT 1,						/* the value of the upvote (if we intend to use an upvoting algorithm based on the partner reputation) */
+	value SMALLINT DEFAULT 1,							/* the value of the upvote (if we intend to use an upvoting algorithm based on the partner reputation) */
 	/* - */
 	created_at TIMESTAMP WITH TIME ZONE,
 	updated_at TIMESTAMP WITH TIME ZONE
