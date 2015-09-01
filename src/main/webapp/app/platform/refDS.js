627db8862180db1764d4ace6b3968f59d0ebf2a1
Lucian Feroiu <lucianferoiu@gmail.com>
2015-09-01 21:17:54 +0300

    Formatting..

--- src/main/webapp/app/platform/refDS.js
+++ src/main/webapp/app/platform/refDS.js
@@ -1,11 +1,11 @@
 (function() {
 	angular.module('util.ref',[])
 		.service('refDS',['$q','$http',refDS]);//we want a new instance per client (so we use service instead of factory) so we can maintain state inside it...
-		
+
 	function refDS($q,$http) {
 		var ds = this;
 		ds.cache = {};
-		
+
 		return {
 			preload: preload,
 			languages: getLanguages,
@@ -29,18 +29,18 @@
 							var lang = data[i];
 							ds.cache.langs[lang.code] = lang;
 						}
-					
+
 						if (fn) {
 							fn(data);
 						}
-					}) 
+					})
 					.error(function(data, status, headers, config) {
 						delete ds.cache.langs;
 						console.log('cannot retrieve the languages list..' + data);
 					});
 			}
 		}
-		
+
 		function getTags(force,fn) {
 			if (ds.cache.tags && !force) {
 				if (fn) {
@@ -55,14 +55,14 @@
 						if (fn) {
 							fn(data);
 						}
-					}) 
+					})
 					.error(function(data, status, headers, config) {
 						delete ds.cache.tags;
 						console.log('cannot retrieve the list of tags..' + data);
 					});
 			}
 		}
-		
+
 		function getCurrentUser(force,fn) {
 			if (ds.cache.crtUser && !force) {
 				if (fn) {
@@ -77,21 +77,20 @@
 						if (fn) {
 							fn(data);
 						}
-					}) 
+					})
 					.error(function(data, status, headers, config) {
 						delete ds.cache.crtUser;
 						console.log('cannot retrieve the current user..' + data);
 					});
 			}
-			
 		}
-		
+
 		function preload(force) {
 			getLanguages(force);
 			getTags(force);
 			getCurrentUser(force);
 		}
-		
+
 		function newRange(n) {
 			return new Array(n);
 		}
